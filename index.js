7b41269375d95a0e47be8a24d570a880ea0a1174
JasonAkiraJordan <jasonajordan0@gmail.com>
2020-12-09 18:25:28 -0500

    asdf

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+
+function writeCards(array, string){
+    let newarr = []
+    for (let i = 0 ; i< array.length; i++){
+        newarr.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+
+    }
+    return newarr;
+}
+
+
+function countDown(num){
+    let num2 = num
+    while (num2 >= 0) {
+        console.log(num2)
+        num2 = num2 - 1;
+    }
+
+}
+
